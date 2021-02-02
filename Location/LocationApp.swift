9a1c18b31597d9c69fc0ecf28efa9bc469e0fecb
Etienne Vautherin <etienne.vautherin@orange.com>
2021-02-02 11:22:09 +0100

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Etienne Vautherin on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
