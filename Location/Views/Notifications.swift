7d3b952a301254cdb1de824c5c23b59191962a60
Etienne Vautherin <etienne.vautherin@orange.com>
2021-02-04 15:50:37 +0100

    Notification request (10s)

--- Location/Views/Notifications.swift
+++ Location/Views/Notifications.swift
@@ -7,13 +7,30 @@
 
 import Foundation
 import SwiftUI
+import UserNotifications
 
 
 struct Notifications: View {
 
     var body: some View {
-        Button("Nofication") {
+        Button("Notification") {
+            let content = UNMutableNotificationContent()
+            content.title = "Notification Title"
+            content.body = "Notification body"
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
             
+            // Create the request
+            let uuidString = UUID().uuidString
+            let request = UNNotificationRequest(identifier: uuidString,
+                                                content: content, trigger: trigger)
+
+            // Schedule the request with the system.
+            let notificationCenter = UNUserNotificationCenter.current()
+            notificationCenter.add(request) { (error) in
+               if error != nil {
+                  // Handle any errors.
+               }
+            }
         }
     }
 }
