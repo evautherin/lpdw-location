eb5c897afe119bb2355404c78bb94965bf8fc673
Etienne Vautherin <etienne.vautherin@orange.com>
2021-02-05 10:04:12 +0100

    Always willPresent notification

--- Location/Models/NotificationsDelegate.swift
+++ Location/Models/NotificationsDelegate.swift
@@ -12,7 +12,10 @@ import UserNotifications
 class NotificationsDelegate: NSObject {
     
     override init() {
+        super.init()
+        
         let center = UNUserNotificationCenter.current()
+        center.delegate = self
         center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             
             // Code du siècle dernier
@@ -44,4 +47,14 @@ class NotificationsDelegate: NSObject {
         }
     }
 
+}
+
+
+extension NotificationsDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.badge, .banner, .list, .sound])
+    }
+    
 }
